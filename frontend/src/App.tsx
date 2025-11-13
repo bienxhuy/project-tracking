31a57c3b41b435dfeca0d10c2924bbd4522ad9c8
Bien Xuan Huy <bienxhuy@gmail.com>
2025-11-13 15:27:25 +0700

    feat: implement MainLayout component and integrate routing in App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import MainLayout from "./components/MainLayout"
 import { TempPage } from "./pages/TempPage"
 
 function App() {
   return (
     <>
-      <TempPage />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/temp" element={<TempPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
