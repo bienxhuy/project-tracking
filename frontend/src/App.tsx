c129c861e92d65b543577e4bea6f1deb104278ff
Bien Xuan Huy <bienxhuy@gmail.com>
2025-11-28 00:18:36 +0700

    feat: Add instructor dashboard with project management features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { MainLayout } from "./pages/MainLayout"
 import { TempPage } from "./pages/TempPage"
 import { StudentDashboard } from "./pages/student/StudentDashboard"
+import { InstructorDashboard } from "./pages/instructor/InstructorDashboard"
+import { ProjectEditorPage } from "./pages/instructor/ProjectEditorPage"
 import { ProjectDetailPage } from "./pages/ProjectDetail"
 import { MilestoneDetailPage } from "./pages/MilestoneDetail"
 import { ScrollToTop } from "./components/ScropToTop"
 import { TaskDetailPage } from "./pages/TaskDetail"
+import { Toaster } from "@/components/ui/sonner"
 
 function App() {
   return (
@@ -19,6 +22,12 @@ function App() {
               <Route path="dashboard" element={<StudentDashboard />} />
             </Route>
 
+            <Route path="instructor">
+              <Route path="dashboard" element={<InstructorDashboard />} />
+              <Route path="project/create" element={<ProjectEditorPage />} />
+              <Route path="project/edit/:projectId" element={<ProjectEditorPage />} />
+            </Route>
+
             <Route path="project/:id" element={<ProjectDetailPage />} />
             <Route path="project/:projectId/milestone/:milestoneId" element={<MilestoneDetailPage />} />
             <Route path="project/:projectId/milestone/:milestoneId/task/:taskId" element={<TaskDetailPage />} />
@@ -27,6 +36,7 @@ function App() {
           <Route path="/temp" element={<TempPage />} />
         </Routes>
       </BrowserRouter>
+      <Toaster />
     </>
   )
 }
